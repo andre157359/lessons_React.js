5642ea3ae9815353f6302a67c9ec692d400ca1d4
Andrey Rybin <andre157359@yandex.ru>
2019-09-14 19:01:58 +0300

    v_0.5

--- lesson-3/src/components/app/app.js
+++ lesson-3/src/components/app/app.js
@@ -1,16 +1,23 @@
 import React from 'react';
+
 import AppHeader from '../app-header';
 import SearchPannal from '../search-panel';
-import PostStatysFilter from '../post-status-filter'
+import PostStatysFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+import './app.css';
 
 const App = () => {
     return ( 
         <div className = "app">
             <AppHeader/> 
-          <div className = "serc-pannal d-flex">
+          <div className = "serch-pannal d-flex">
               <SearchPannal/> 
               <PostStatysFilter/>
           </div>
+          <PostList/>
+          <PostAddForm/>
         </div>
         
         
