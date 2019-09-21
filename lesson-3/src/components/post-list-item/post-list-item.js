c032eb7b05d8a54fa9b649e81841079ba54ccd98
Andrey Rybin <andre157359@yandex.ru>
2019-09-21 23:16:32 +0300

    v_0.7_lesson_6

--- lesson-3/src/components/post-list-item/post-list-item.js
+++ lesson-3/src/components/post-list-item/post-list-item.js
@@ -3,32 +3,10 @@ import React, {Component} from 'react';
 import './post-list-item.sass'
 
 export default class PostListItem extends Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            important: false,
-            like: false
-        };
-        this.onImportant = this.onImportant.bind(this);
-        this.onLike = this.onLike.bind(this);
 
-    }
-
-    onImportant () {
-        this.setState(({important}) => ({
-            important: !important
-        }))
-    }
-
-    onLike() {
-        this.setState(({like}) => ({
-            like: !like
-        }))
-    }
 
     render() {
-        const {lable, onDelete} = this.props;
-        const {important, like} = this.state;
+        const {lable, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props;
         let classNames = 'app-list-item d-flex justify-content-between';
         if (important) {
             classNames += ' important';
@@ -41,14 +19,14 @@ export default class PostListItem extends Component {
             <div className = {classNames}>
              <span 
                className = "app-list-item-lable"
-               onClick = {this.onLike}>
+               onClick = {onToggleLiked}>
                 {lable}
              </span>
             <div className = "d-flex justify-content-center align-items-center">
                 <button 
                  type = "button" 
                  className = "btn-star btn-sm"
-                 onClick = {this.onImportant}>
+                 onClick = {onToggleImportant}>
                     <i className = " fa fa-star"></i>
                 </button>
                 <button type = "button"
