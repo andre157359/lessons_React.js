c2438b998e341e0009eeeed345c74e2105ee8ef5
Andrey Rybin <andre157359@yandex.ru>
2019-09-22 14:47:37 +0300

    lesson_7_v_0.1

---
+++ lesson-7/script.js
@@ -0,0 +1,31 @@
+class GotService {
+
+    constructor() {
+        this._apiBase = 'https://www.anapioficeandfire.com/api'
+    }
+
+     async getResourse(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+        if(!res.ok) {
+            throw new Error (`Could not featch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    };   
+    getAllCharacterc() {
+        return this.getResourse('/characters?page=5&pageSize=10');
+    } 
+    getCharacterc(id) {
+        return this.getResourse(`/characters/${id}`);
+    }
+}
+
+const got =new GotService ();
+
+got.getAllCharacterc()
+    .then(res => {
+        res.forEach(item => console.log(item.name));
+    });
+
+got.getCharacterc(130)
+    .then(res => console.log(res));
