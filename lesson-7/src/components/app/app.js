d3cc994700245d64caac89d604239da02ad4d3cd
Andrey Rybin <andre157359@yandex.ru>
2019-10-09 21:21:34 +0300

    lesson_9_v_0.2

--- lesson-7/src/components/app/app.js
+++ lesson-7/src/components/app/app.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
-import ItemList from '../itemList';
-import CharDetails from '../charDetails';
+import ErrorMessage from '../errorMessage';
+import CharacterPage from '../characterPage';
 // import BtnClose from '../btn_close';
 
 import './app.css';
@@ -13,8 +13,16 @@ export default class App extends Component {
 
     state = {
         showRandomChar:true,
-        selectedChar: 25
+        error: false
     };
+
+    componentDidCatch() {
+        console.log('error');
+        this.setState({
+            error: true
+        })
+        
+    }
 
     toggleRandomChar = () => {
         this.setState((state) => {
@@ -24,15 +32,14 @@ export default class App extends Component {
         });
     };
 
-    onCharSelected = (id) => {
-        this.setState({
-            selectedChar: id
-
-        })
-    }
+   
 
     render() {
         const char = this.state.showRandomChar ? <RandomChar/> : null;
+
+        if (this.state.error) {
+            return <ErrorMessage/>
+        }
 
         return (
                     <> 
@@ -49,14 +56,9 @@ export default class App extends Component {
                                         >Toggle random character</button>
                                 </Col>
                             </Row>
-                            <Row>
-                                <Col md='6'>
-                                    <ItemList onCharSelected = {this.onCharSelected}/>
-                                </Col>
-                                <Col md='6'>
-                                    <CharDetails charId = {this.state.selectedChar}/>
-                                </Col>
-                            </Row>
+                            <CharacterPage/>
+                            <CharacterPage/>
+                            <CharacterPage/>
                         </Container>
                     </>
     
